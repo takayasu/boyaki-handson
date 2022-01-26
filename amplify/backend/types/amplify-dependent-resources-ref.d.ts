21d7a5222b202bbdbc9eb3e32a53f0d3f10e5f69
Atsushi Takayasu <atsushi.takayasu@bigtreetc.com>
2022-01-26 15:36:58 +0900

    amplify init/add auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "boyaki2fd1058bc": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
